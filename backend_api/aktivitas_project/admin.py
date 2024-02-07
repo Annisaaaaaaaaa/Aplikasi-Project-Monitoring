16fe5fdefabe905be0941c04d5c35d348e88ca1f
AiHateThisUniverse <irahumanitas@gmail.com>
2024-02-07 16:06:19 +0700

    Workload dan Aktivitas Projek

--- backend_api/aktivitas_project/admin.py
+++ backend_api/aktivitas_project/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from import_export import resources
+from .models import AktivitiesProject
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+class ActivityResource(resources.ModelResource):
+    class Meta:
+        model = AktivitiesProject
+
+class AcitivtyAdmin(ImportExportModelAdmin):
+    resource_class = ActivityResource
+    list_display = ['name', 'project', 'status', 'created_at']
+    list_filter = ('status',)
+    search_fields = ('name', 'project', 'status')
+    ordering = ['-created_at', 'name']
+
+admin.site.register(AktivitiesProject, AcitivtyAdmin)
